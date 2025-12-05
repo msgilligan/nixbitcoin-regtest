3a4f287996cceaa3e11f71ec1c5d4fa9e92ea00c
Sean Gilligan <sean@msgilligan.com>
2025-12-05 06:31:07 -0800

    GenerateBlock.java: Remove redundant/transitive DEPS line

--- scripts/GenerateBlock.java
+++ scripts/GenerateBlock.java
@@ -1,7 +1,6 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
 //JAVA 25+
 //REPOS mavencentral, consensusj=https://gitlab.com/api/v4/projects/8482916/packages/maven
-//DEPS org.bitcoinj:bitcoinj-core:0.17
 //DEPS com.msgilligan:cj-btc-jsonrpc:0.7.0-alpha3
 //DEPS org.slf4j:slf4j-jdk14:2.0.17
 
